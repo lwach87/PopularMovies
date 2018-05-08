deada962e2ba8039066a066d258ec5044c29729e
Łukasz Wachowski <lwach87@gmail.com>
2018-05-08 14:52:02 +0200

    DetailActivity upgraded

--- app/src/main/java/com/example/lukaszwachowski/popularmovies/configuration/NetworkUtils.java
+++ app/src/main/java/com/example/lukaszwachowski/popularmovies/configuration/NetworkUtils.java
@@ -4,11 +4,14 @@ public class NetworkUtils {
 
     public static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
     public static final String IMAGE_URL = "http://image.tmdb.org/t/p/w185/";
-    public static final String API_KEY = "";
+    public static final String YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v=";
+    public static final String YOUTUBE_IMG = "https://img.youtube.com/vi/";
+    public static final String API_KEY = "26ae9389aea811fc3942a97a387102e7";
 
     public static final String ORIGINAL_TITLE = "title";
     public static final String MOVIE_POSTER = "poster";
     public static final String PLOT_SYNOPSIS = "plot";
     public static final String USER_RATING = "rating";
     public static final String RELEASE_DATE = "date";
+    public static final String MOVIE_ID = "id";
 }
--- app/src/main/java/com/example/lukaszwachowski/popularmovies/ui/detailActivity/DetailActivity.java
+++ app/src/main/java/com/example/lukaszwachowski/popularmovies/ui/detailActivity/DetailActivity.java
@@ -1,22 +1,51 @@
 package com.example.lukaszwachowski.popularmovies.ui.detailActivity;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 import com.example.lukaszwachowski.popularmovies.MoviesApp;
+import com.example.lukaszwachowski.popularmovies.R;
 import com.example.lukaszwachowski.popularmovies.di.components.DaggerDetailActivityComponent;
 import com.example.lukaszwachowski.popularmovies.di.modules.DetailActivityModule;
+import com.example.lukaszwachowski.popularmovies.network.reviews.ReviewsResult;
+import com.example.lukaszwachowski.popularmovies.network.videos.VideosResult;
 
 import javax.inject.Inject;
 
-public class DetailActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailActivity extends AppCompatActivity implements DetailActivityMVP.View {
 
     @Inject
     DetailAdapter detailAdapter;
 
+    @Inject
+    ReviewListAdapter reviewsAdapter;
+
+    @Inject
+    VideoListAdapter videosAdapter;
+
+    @Inject
+    DetailActivityMVP.Presenter presenter;
+
+    @BindView(R.id.reviews_recycler_view)
+    RecyclerView reviewRecyclerView;
+
+    @BindView(R.id.videos_recycler_view)
+    RecyclerView videoRecyclerView;
+
+    @BindView(R.id.activity_detail)
+    ViewGroup rootView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getSupportActionBar().setTitle("Detail Activity");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -26,5 +55,54 @@ public class DetailActivity extends AppCompatActivity {
                 .build().inject(this);
 
         setContentView(detailAdapter);
+
+        ButterKnife.bind(this);
+
+        videoRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        videoRecyclerView.setNestedScrollingEnabled(false);
+        videoRecyclerView.setHasFixedSize(true);
+        videoRecyclerView.setAdapter(videosAdapter);
+
+        reviewRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        reviewRecyclerView.setNestedScrollingEnabled(false);
+        reviewRecyclerView.setHasFixedSize(true);
+        reviewRecyclerView.setAdapter(reviewsAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.setView(this);
+
+        videosAdapter.clearData();
+
+        String movieId = String.valueOf(getIntent().getIntExtra("id", 1));
+        presenter.loadReviews(movieId);
+        presenter.loadVideos(movieId);
+    }
+
+
+    @Override
+    public void updateReviews(ReviewsResult result) {
+        reviewsAdapter.swapData(result);
+    }
+
+    @Override
+    public void updateVideos(VideosResult result) {
+        videosAdapter.swapData(result);
+    }
+
+    @Override
+    public void showSnackBar() {
+        Snackbar.make(rootView, "No internet connection", Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        reviewsAdapter.clearData();
+        videosAdapter.clearData();
+        presenter.reviewsUnSubscribe();
+        presenter.videosUnSubscribe();
     }
 }
